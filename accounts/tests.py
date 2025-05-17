8c4ddd42634baa6d9b62d4e69b9095f2dbed7a55
erfan <eazari.dev@gmail.com>
2025-05-17 09:50:21 +0330

    user authentication

--- accounts/tests.py
+++ accounts/tests.py
@@ -2,6 +2,7 @@
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
 
 
@@ -15,7 +16,7 @@ class UsersManagersTests(TestCase):
             password="testpass1234",
         )
         self.assertEqual(user.username, "testuser")
-        self.assertEqual(user.email, "testuser@exampe.com")
+        self.assertEqual(user.email, "testuser@example.com")
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
@@ -34,3 +35,29 @@ class UsersManagersTests(TestCase):
         self.assertTrue(admin_uesr.is_staff)
         self.assertTrue(admin_uesr.is_superuser)
         
+
+class SignupPageTests(TestCase):
+    
+    def test_url_exists_at_correct_location_signupview(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
+        
+    def test_signup_view_name(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/signup.html")
+        
+    def test_signup_form(self):
+        response = self.client.post(
+            reverse("signup"),{
+                "username": "testuser",
+                "email": "testuser@email.com",
+                "password1": "testpass123",
+                "password2": "testpass123",
+        },)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, "testuser")
+        self.assertEqual(
+            get_user_model().objects.all()[0].email, "testuser@email.com"
+        )
